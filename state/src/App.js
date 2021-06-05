136f81beacee71597c2ae590e1130fbf354b874b
Clement Buchanan <clement.buchanan@gmail.com>
2021-06-05 16:59:41 -0400

    Modal.Header was mispelled causing error message when clicked on persons name. This prevented Modal from running

--- state/src/App.js
+++ state/src/App.js
@@ -1,10 +1,11 @@
 import AgeWithClass from './age-class.js';
 import AgeWithFunction from './age-function.js';
 import Family from './family.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Family people={['Lincoln', 'Karen', 'Clement',
         'Audrey']} />
       <AgeWithClass age={50} />
--- state/src/family.js
+++ state/src/family.js
@@ -1,22 +1,43 @@
 import React, { useState } from 'react';
-import { ListGroup } from 'react-bootstrap';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+
 
 function Family(props) {
+  const [activePerson, setActivePerson] = useState('');
   const [family, setFamily] = useState(props.people);
   const [modalOpen, setModalOpen] = useState(false);
 
   const showModal = (person) => {
-    console.log('person');
+    setActivePerson(person);
+    // setFamily(person);
+    setModalOpen(true);
+  }
+
+  const handleClose = () => {
+    setModalOpen(false);
   }
 
   return (
-    <ListGroup horizontal>
-      {
-        family.map((person =>
-          <ListGroup.Item key={person} onClick={() => showModal(person)}>{person}</ListGroup.Item>
-        ))
-      }
-    </ListGroup>
+    <>
+      <ListGroup horizontal>
+        {
+          family.map((person =>
+            <ListGroup.Item key={person} onClick={() => showModal(person)}>{person}</ListGroup.Item>
+          ))
+        }
+      </ListGroup>
+      <Modal show={modalOpen} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal Heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Yes Rasta! You clicked on {activePerson}</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>Close</Button>
+        </Modal.Footer>
+      </Modal >
+    </>
   )
 }
 export default Family;
