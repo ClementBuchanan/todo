931d748ee6abcdee99f8f1fdb7e9bed4d9e95c9a
Clement Buchanan <clement.buchanan@gmail.com>
2021-06-05 11:36:56 -0400

    ACP for lab-31

--- state/src/App.js
+++ state/src/App.js
@@ -1,9 +1,12 @@
 import AgeWithClass from './age-class.js';
 import AgeWithFunction from './age-function.js';
+import Family from './family.js';
 
 function App() {
   return (
     <div className="App">
+      <Family people={['Lincoln', 'Karen', 'Clement',
+        'Audrey']} />
       <AgeWithClass age={50} />
       <AgeWithFunction age={34} />
     </div>
--- state/src/age-function.js
+++ state/src/age-function.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import { Button } from 'react-bootstrap';
+
 
 function Age(props) {
 
@@ -18,7 +20,7 @@ function Age(props) {
     setEither(!either);
   }
 
-  //This will simulate when the componentMounted... runs ONLY ONCE on the 1st run
+  //This will simulate when the componentMounts... runs ONLY ONCE on the 1st run
   useEffect(() => {
     console.log('this runs everytime the component mounts/runs ONLY ONCE!')
   }, []);
@@ -37,24 +39,28 @@ function Age(props) {
 
   //This one runs  either when 'age' changes or when 'changeExpression' changes.
   useEffect(() => {
+    console.log('This runs when facial expression changes')
+  }, [smiling]);
+
+  //This one runs  either when 'age' changes or when 'changeExpression' changes.
+  //will use this all the time
+  useEffect(() => {
     console.log('This runs when either age or facial expression changes')
   }, [age, smiling]);
 
-  //This one will simulate when the componentUnmount()
+  //This one will simulate when the componentUnmounst()
+  //will run before the next render cycle starts
   useEffect(() => {
     return () => console.log('The component unmounted. GOOD BYE')
-  }, [])
+  })
 
   return (
     <>
-      <h2>My age is: {age}, and I am smiling about it {smiling.toString()}</h2>
-      <button onClick={changeAge}>Change Age</button>
-      <button onClick={changeExpression}>Change Expression</button>
-      <button onClick={changeEither}>Change Either</button>
+      <h2>My age is: {age}, and I am smiling about it. {smiling.toString()}</h2>
+      <Button onClick={changeAge}>Change Age</Button>
+      <Button onClick={changeExpression}>Change Expression</Button>
+      <Button onClick={changeEither}>Change Either</Button>
     </>
   )
 }
-
-
-
 export default Age;
