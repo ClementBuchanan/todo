7683722602a40f0e98319916a563c4a4522c1811
Clement Buchanan <clement.buchanan@gmail.com>
2021-06-10 21:20:26 -0400

    1st ACP for lab 32

--- state/src/App.js
+++ state/src/App.js
@@ -1,22 +1,41 @@
 import React from 'react';
-import AgeWithClass from './age-class.js';
-import AgeWithFunction from './age-function.js';
-import Family from './family.js';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 import ToDo from './components/todo/todo.js';
 
-
-function App() {
-  return (
-    <div>
-      <Family people={['Lincoln', 'Karen', 'Clement',
-        'Audrey']} />
-      <AgeWithClass age={50} />
-      <AgeWithFunction age={34} />
-      <ToDo />
-    </div>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <>
+        <ToDo />
+      </>
+    );
+  }
 }
 
-export default App;
+
+
+
+// import React from 'react';
+// import AgeWithClass from './age-class.js';
+// import AgeWithFunction from './age-function.js';
+// import Family from './family.js';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+// import ToDo from './components/todo/todo.js';
+
+
+// function App() {
+//   return (
+//     <div>
+//       <Family people={['Lincoln', 'Karen', 'Clement',
+//         'Audrey']} />
+//       <AgeWithClass age={50} />
+//       <AgeWithFunction age={34} />
+//       <ToDo />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
--- state/src/family.js
+++ state/src/family.js
@@ -1,43 +1,43 @@
-import React, { useState } from 'react';
-import ListGroup from 'react-bootstrap/ListGroup';
-import Modal from 'react-bootstrap/Modal';
-import Button from 'react-bootstrap/Button';
+// import React, { useState } from 'react';
+// import ListGroup from 'react-bootstrap/ListGroup';
+// import Modal from 'react-bootstrap/Modal';
+// import Button from 'react-bootstrap/Button';
 
 
-function Family(props) {
-  const [activePerson, setActivePerson] = useState('');
-  const [family, setFamily] = useState(props.people);
-  const [modalOpen, setModalOpen] = useState(false);
+// function Family(props) {
+//   const [activePerson, setActivePerson] = useState('');
+//   const [family, setFamily] = useState(props.people);
+//   const [modalOpen, setModalOpen] = useState(false);
 
-  const showModal = (person) => {
-    setActivePerson(person);
-    // setFamily(person);
-    setModalOpen(true);
-  }
+//   const showModal = (person) => {
+//     setActivePerson(person);
+//     // setFamily(person);
+//     setModalOpen(true);
+//   }
 
-  const handleClose = () => {
-    setModalOpen(false);
-  }
+//   const handleClose = () => {
+//     setModalOpen(false);
+//   }
 
-  return (
-    <>
-      <ListGroup horizontal>
-        {
-          family.map((person =>
-            <ListGroup.Item key={person} onClick={() => showModal(person)}>{person}</ListGroup.Item>
-          ))
-        }
-      </ListGroup>
-      <Modal show={modalOpen} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal Heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Yes Rasta! You clicked on {activePerson}</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>Close</Button>
-        </Modal.Footer>
-      </Modal >
-    </>
-  )
-}
-export default Family;
+//   return (
+//     <>
+//       <ListGroup horizontal>
+//         {
+//           family.map((person =>
+//             <ListGroup.Item key={person} onClick={() => showModal(person)}>{person}</ListGroup.Item>
+//           ))
+//         }
+//       </ListGroup>
+//       <Modal show={modalOpen} onHide={handleClose}>
+//         <Modal.Header closeButton>
+//           <Modal.Title>Modal Heading</Modal.Title>
+//         </Modal.Header>
+//         <Modal.Body>Yes Rasta! You clicked on {activePerson}</Modal.Body>
+//         <Modal.Footer>
+//           <Button variant="secondary" onClick={handleClose}>Close</Button>
+//         </Modal.Footer>
+//       </Modal >
+//     </>
+//   )
+// }
+// export default Family;
