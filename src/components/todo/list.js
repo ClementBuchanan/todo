df90fe93e1f618f8fcae2226888ed8ee1531d502
Clement Buchanan <clement.buchanan@gmail.com>
2021-06-25 15:54:49 -0400

    completed display and hide feature

--- src/components/todo/list.js
+++ src/components/todo/list.js
@@ -1,24 +1,33 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Context } from '../../context.js';
+
 export default function ToDosList(props) {
+  const context = useContext(Context);
   console.log(props.list);
 
   return (
     <>
       <h1>What's the plan for today?</h1>
       <ul>
-        {props.list.map(item => (
-          <li
-            className={`complete-${item.complete.toString()}`}
-            key={item._id}
-          >
-            <span onClick={() => props.handleComplete(item)}>
-              {item.text}
-            </span>
-            <button onClick={() => props.handleDelete(item._id)}>
-              Delete
-            </button>
-          </li>
-        ))}
+        {props.list.map((item) => {
+          console.log(context.showCompleted, item.complete);
+          if (!context.showCompleted && item.complete) {
+            return null;
+          }
+          return (
+            <li
+              className={`complete-${item.complete.toString()}`}
+              key={item._id}
+            >
+              <span onClick={() => props.handleComplete(item)}>
+                {item.text}
+              </span>
+              <button onClick={() => props.handleDelete(item._id)}>
+                Delete
+              </button>
+            </li>
+          );
+        })}
       </ul>
     </>
   );
